22ac709577e43388d431e792d3032562ed590f99
github_Chaitanya17414 <chaitanyachinta17414@gmail.com>
2023-05-15 23:15:55 +0530

    React App initial steps

---
+++ secondapp/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Carousel from './components/Carousel';
+
+function App() {
+  return (
+    <div >
+     <Header />
+     <Carousel />
+     <Footer />
+    </div>
+  );
+}
+
+export default App;
